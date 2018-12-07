d7f5f43d4a9e70a91138b444c9c3aa2b64b1f98e
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2018-12-07 10:18:04 -0200

    update app index for test

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import Home from './Pages/Home'
 import Footer from './Components/footer'
 import { withRouter } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment> 
-        <BrowserRouter>
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/form' component= {Complaint}/> 
             <Footer />
           </Switch>
-        </BrowserRouter>
       </React.Fragment>
-
     );
   }
 }
